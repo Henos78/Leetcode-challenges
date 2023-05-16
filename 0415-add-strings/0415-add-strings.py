641b39558b124625b745d8553595703fbd8f9d8a
Henos78 <90530676+Henos78@users.noreply.github.com>
2023-05-16 07:53:13 +0200

    Time: 46 ms (42.34%), Space: 16.4 MB (14.03%) - LeetHub

--- 0415-add-strings/0415-add-strings.py
+++ 0415-add-strings/0415-add-strings.py
@@ -1,5 +1,6 @@
 class Solution:
     def addStrings(self, num1: str, num2: str) -> str:
+        #not the correct result based on the question prompt
         sys.set_int_max_str_digits(10000)
         num1 = int(num1)
         num2 = int(num2)
