2324abbda929f1dec49b194dd58202e1156a9b2c
Henos78 <90530676+Henos78@users.noreply.github.com>
2023-05-18 22:02:55 +0200

    Time: 999 ms (41.81%), Space: 19.6 MB (13.88%) - LeetHub

--- 0318-maximum-product-of-word-lengths/0318-maximum-product-of-word-lengths.py
+++ 0318-maximum-product-of-word-lengths/0318-maximum-product-of-word-lengths.py
@@ -1,13 +1,16 @@
 class Solution:
     def maxProduct(self, words: List[str]) -> int:
+        #using bit
         maxpro= 0
         temp = [0]*len(words)
         
+        #here we convert the words into a bit representation using their ascii value
         for i in range(len(words)):
             for ch in words[i]:
                 temp[i] |= 1 << (ord(ch)-ord('a'))
 
-     
+        #here we itrate through the temp array and select two words and do AND operation and if = to 0 it means they have no common characters we will multiply and store the res and Continue doing that and find the max product 
+        
         for i in range(len(words)):
             for j in range(1, len(words)):
                 if temp[i]&temp[j] == 0:
