5a7b5197e684463b62e5e379d1fa5a32f2ee2a72
Henos78 <90530676+Henos78@users.noreply.github.com>
2023-06-07 21:21:19 +0200

    Time: 91 ms (14.10%), Space: 17.9 MB (13.99%) - LeetHub

--- 0120-triangle/0120-triangle.py
+++ 0120-triangle/0120-triangle.py
@@ -1,11 +1,17 @@
 class Solution:
     def minimumTotal(self, triangle: List[List[int]]) -> int:
+        #dp with memoraization top down approach
         n = len(triangle)
-        @lru_cache
+        
+        dp = [[-1]*len(triangle)  for _ in range(len(triangle))]
+        
         def helper(i,j):
             # i-row and j - col
             if n-1 == i:
                 return triangle[i][j]
+            
+            if dp[i][j] != -1:
+                return dp[i][j]
 
             p1 = helper(i+1,j)
             p2 = helper(i+1,j+1)
@@ -13,6 +19,7 @@ class Solution:
             t1 = triangle[i][j] + p1
             t2 = triangle[i][j] + p2
 
-            return min(t1, t2)
+            dp[i][j] = min(t1, t2)
+            return dp[i][j]
 
         return helper(0,0)
