d5a284d23e357ad0edf0a3c2f0fa7909d821d5fd
Henos78 <90530676+Henos78@users.noreply.github.com>
2023-06-16 21:41:48 +0200

    Time: 59 ms (37.78%), Space: 16.3 MB (73.63%) - LeetHub

--- 0377-combination-sum-iv/0377-combination-sum-iv.py
+++ 0377-combination-sum-iv/0377-combination-sum-iv.py
@@ -2,12 +2,12 @@ class Solution:
     def combinationSum4(self, nums: List[int], target: int) -> int:
         dp =[0]*(target+1)
         
-    
-        for i in range(target+1):
+        dp[0]=1
+        for i in range(1,target+1):
             for num in nums:
                 temp =i-num
-                if num==i:
-                    dp[i]+=1
+                # if num==i:
+                #     dp[i]+=1
                 if  temp>=0:
                     dp[i] += dp[temp]
              
