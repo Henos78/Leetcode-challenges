a573d9b71689aae852bf6b359fa1efaa5571e920
Henos78 <90530676+Henos78@users.noreply.github.com>
2023-06-21 21:14:10 +0200

    Time: 1551 ms (99.31%), Space: 62.6 MB (55.92%) - LeetHub

--- 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.py
+++ 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.py
@@ -1,22 +1,22 @@
 class Solution:
     def mostPoints(self, questions: List[List[int]]) -> int:
-        #using top down
-        dp  =[-1]*len(questions)
+        #using bottom up
+        n =len(questions)
+        dp  =[-1]*n
         
-        def solve(i):
-            if i>=len(questions):
-                return 0
+        dp[-1] = questions[-1][0]
+        
+        for i in range(n-2,-1,-1):
+            pick_idx =  i+questions[i][1]+1
+            pick = questions[i][0]
             
-            if dp[i] != -1:
-                return dp[i]
+            if pick_idx <n:
+                pick += dp[pick_idx]
+                
+            not_pick = dp[i+1]
             
-            pick = questions[i][0] + solve(i+questions[i][1]+1)
-            not_pick = solve(i+1)
-            dp[i]=max(pick,not_pick)
-                         
-            return dp[i]
-      
-        return solve(0)
+            dp[i] = max(pick,not_pick)
             
+        return dp[0]
         
         
